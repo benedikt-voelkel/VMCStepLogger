b54f92dad613b2ae9028dac1dd2d780d955d61a0
Benedikt Volkel <benedikt.volkel@cern.ch>
2019-06-10 13:21:35 +0000

    Comply with renamed O2 libs

--- plugins/O2StepCountIntercept.cxx
+++ plugins/O2StepCountIntercept.cxx
@@ -37,15 +37,33 @@ class FairMCApplication
   void FinishRun();
 };
 
+namespace o2
+{
+namespace data
+{
+class Stack
+{
+  public:
+   void addHit(int iDet);
+};
+}
+}
+
 // Put into namespace since that could be a common name for a variable
 namespace simplestepcount
 {
   int stepCount = 0;
+  int hitCount = 0;
 }
 
 void countStep()
 {
   simplestepcount::stepCount++;
+}
+
+void countHit()
+{
+  simplestepcount::hitCount++;
 }
 
 void FairMCApplication::Stepping()
@@ -55,9 +73,17 @@ void FairMCApplication::Stepping()
   o2::mcsteploggerutilities::dispatchOriginalKernel<FairMCApplication, StepMethodType>(this, "libBase", "_ZN17FairMCApplication8SteppingEv");
 }
 
+void o2::data::Stack::addHit(int iDet)
+{
+  countHit();
+  using StepMethodType = void (o2::data::Stack::*)(int);
+  o2::mcsteploggerutilities::dispatchOriginalKernel<o2::data::Stack, StepMethodType>(this, "libO2SimulationDataFormat", "_ZN2o24data5Stack6addHitEi", iDet);
+}
+
 void FairMCApplication::FinishRun()
 {
   std::cerr << "TOTAL STEPS " << simplestepcount::stepCount << std::endl;
+  std::cerr << "TOTAL HITS " << simplestepcount::hitCount << std::endl;
   using StepMethodType = void (FairMCApplication::*)();
   o2::mcsteploggerutilities::dispatchOriginalKernel<FairMCApplication, StepMethodType>(this, "libBase", "_ZN17FairMCApplication9FinishRunEv");
 }
